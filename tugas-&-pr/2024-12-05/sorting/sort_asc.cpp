2090f8888f7fcbc2902d6d19b5efdbce2bfe65c1
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2024-12-09 09:55:44 +0700

    Sorting
    sorting

---
+++ tugas-&-pr/2024-12-05/sorting/sort_asc.cpp
@@ -0,0 +1,48 @@
+// Nama program   : sort_asc.cpp
+// Deskripsi      : sort dari terkecil ke terbesar
+// Pembuat        : Ikhsan Satriadi / 241511080
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int A[6], i, j, temp;
+
+  // input
+  i = 1;
+  while (i <= 5)
+  {
+    cout << "A[" << i << "] = ";
+    cin >> A[i];
+    i = i + 1;
+  }
+
+  // sorting
+  i = 1;
+  while (i <= 4)
+  {
+    j = i + 1;
+    while (j <= 5)
+    {
+      if (A[j] < A[i])
+      {
+        temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+      }
+      j = j + 1;
+    }
+    i = i + 1;
+  }
+
+  // tampilkan
+  i = 1;
+  while (i <= 5)
+  {
+    cout << "\nA[" << i << "] = " << A[i];
+    i = i + 1;
+  }
+
+  return 0;
+}
